3daf2811cdee1cb118a0c13b8c81127322f5bff4
unknown <Alikhan7273@gmail.com>
2020-03-03 23:23:10 -0600

    added Session

--- api/server.js
+++ api/server.js
@@ -10,9 +10,23 @@ const commentRouter = require('../data/routers/commentRouter');
 
 const server = express();
 
+const sessionConfig = {
+    name: 'monster',
+    secret: "Keep it secret, keep it safe!",
+    resave: false,
+    saveUninitialized: true, //related to GDPR compliance
+    cookie: {
+        maxAge: 1000 * 60 * 10,
+        secure: false, // should be true in production
+        httpOnly: true, // true means JS cant touch the cookie
+    },
+};
+
 server.use(helmet());
 server.use(morgan('dev'));
 server.use(cors());
+server.use(session(sessionConfig)); // turn on the session middleware
+//at this point there is a req.session object created by express-session
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
