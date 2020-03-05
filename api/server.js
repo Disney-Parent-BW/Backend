45d9447ba41cc62475dca6aee0ff98a2645bb221
unknown <Alikhan7273@gmail.com>
2020-03-05 00:01:08 -0600

    updated login but still not working

--- api/server.js
+++ api/server.js
@@ -4,10 +4,13 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
 const session = require('express-session'); //npm i express-session to install
+const KnexStore = require('connect-session-knex')(session); // remember to curry and pass the session  // npm install connect-session-knex
 
 const authRouter = require('../data/routers/authRouter');
 const requestRouter = require('../data/routers/requestRouter');
 const commentRouter = require('../data/routers/commentRouter');
+const knex = require('../data/dbConfig.js'); // needed for storing sessions in the database
+
 
 const server = express();
 
@@ -21,6 +24,14 @@ const sessionConfig = {
         secure: false, // should be true in production
         httpOnly: true, // true means JS cant touch the cookie
     },
+    // remember the new keyword
+    store: new KnexStore({
+        knex,
+        tablename: 'sessions',
+        createtable: true,
+        sidfieldname: 'sid',
+        clearInterval: 1000 * 60 * 15,
+    }),
 };
 
 server.use(helmet());
--- data/routers/authRouter.js
+++ data/routers/authRouter.js
@@ -12,7 +12,7 @@ function getJwtToken(id, role){
     role
   };
 
-  const secret = process.env.Jwt || `shhh, it's a secret 🔐`;
+  const secret = process.env.Jwt || `it's a secret`;
 
   const options = {
     expiresIn: '1d'
@@ -72,19 +72,39 @@ router.post('/register', [validateCredentials], (req, res) => {
 }})
 
 // LOG IN USER
-router.post('/login', [validateCredentials], (req, res) => {
-  authDB.findByUsername(user.username)
-    .then(findUser => {
-      if(findUser && bcrypt.compareSync(user.password, findUser.password)){
-        const token = getJwtToken(findUser.user_id, findUser.role);
-        res.status(201).json({ token, 'loggedInUser_id': findUser.user_id, 'username': findUser.username });
-      }else{
-        res.status(401).json({ error: 'Invalid credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json({ error: 'Internal server error: login' });
-    })
-})
+// router.post('/login', [validateCredentials], (req, res) => {
+//   authDB.findByUsername(user.username)
+//     .then(findUser => {
+//       if(findUser && bcrypt.compareSync(user.password, findUser.password)){
+//         const token = getJwtToken(findUser.user_id, findUser.role);
+//         res.status(201).json({ token, 'loggedInUser_id': findUser.user_id, 'username': findUser.username });
+//       }else{
+//         res.status(401).json({ error: 'Invalid credentials' });
+//       }
+//     })
+//     .catch(error => {
+//       res.status(500).json({ error: 'Internal server error: login' });
+//     })
+// })
+
+router.post('/login', (req, res) => {
+    let {username, password} = req.body;
+
+    Users.findBy({username})
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            req.session.loggedIn = true;
+            req.session.username = user.username;
+
+            res.status(200).json({message: `Welcome ${user.username}!`,});
+        }else {
+                res.status(401).json({ message: "Invalid Credentials" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
 
 module.exports = router;
